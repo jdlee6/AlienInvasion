2f902d7da268a965018f253106d211249681291e
joe <jdlee6@buffalo.edu>
2019-08-02 21:54:31 -0400

    display and settings

---
+++ alien_invasion.py
@@ -0,0 +1,33 @@
+''' 
+creating a pygame window and responding to user input
+
+Notes:
+pygame.QUIT - x button of game display windows
+'''
+
+import sys
+import pygame
+from settings import Settings
+
+def run_game():
+    # initialize pygame, settings and screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # start the main loop for the game
+    while True:
+
+        # watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+    
+run_game()
